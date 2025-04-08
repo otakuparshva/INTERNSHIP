599b9f01a2160081f743cd15d3d12e7f92bbc8f4
otakuparshva <parshvashah2004@gmail.com>
2025-04-09 02:09:05 +0530

    chore: simplify Jest configuration by removing unnecessary presets for babel-jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
   },
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   collectCoverageFrom: [
